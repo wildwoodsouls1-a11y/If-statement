3977fe2ed8fe5fd3e696008cc1b39749f12e8bc2
wildwoodsouls1-a11y <wildwoodsouls1@gmail.com>
2026-08-26 00:24:53 +0300

    Add ifstatement example

--- ifstatement.java
+++ ifstatement.java
@@ -1,2 +1,10 @@
-# If-statement
-Learning java if statement
+public class Ifstatement {
+  public static void main (String[] args){
+  int age = 18;
+if (int age >= 18) {
+  System.out.println("You can vote"
+   }  else {                 
+  System.out.println("You can't vote"
+   }                
+  }
+}
